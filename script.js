6e37ffdc48514b2a625f97ff5dfb193d44e6172d
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-21 15:24:32 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function rechache() {
+    let input = document.getElementById("search").value;
+
+    if (input === "") {
+        alert("Tanpri antre non yon ekip oswa yon jwè.");
+    } else {
+        alert("Rezilta rechèch pou: " + input);
+    }
+}
