376908ce9612243cd1865d9f909205561227c0d3
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-22 15:48:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function rechache() {
-    let input = document.getElementById("search").value;
 
-    if (input === "") {
-        alert("Tanpri antre non yon ekip oswa yon jwè.");
-    } else {
-        alert("Rezilta rechèch pou: " + input);
-    }
-}
