1ac1a5d090edfd25ba4bd6f3036eced21f7046f7
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-23 00:09:39 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,11 +1,9 @@
-// ===============================
-// ANALIZFOOT PRO - API SYSTEM
-// ===============================
 const API_URL = "https://v3.football.api-sports.io";
-const API_KEY =c07c38a93b2e98f06baf7069f3467119dbade3caa812e26ca81fd4b8468c7ab9
+const API_KEY = c07c38a93b2e98f06baf7069f3467119dbade3caa812e26ca81fd4b8468c7ab9
 
+async function montreMatch(){
 
-
+let matchBox = document.getElementById("match-api");
 
 try{
 
@@ -18,85 +16,44 @@ headers:{
 }
 );
 
-  
 const data = await response.json();
 
 console.log(data);
 
-  
-catch(error){
+if(!data.response){
 
-console.log("Erè API:", error);
-
-}
-
-  }
-async function montreMatch(){
-console.log("API mache");
-try{
-
-const response = await fetch(
-API_URL + "/fixtures?date=2026-07-23",
-{
-headers:{
-"x-apisports-key": API_KEY
-}
-}
-);
-
-const data = await response.json();
-
-let matchBox = document.getElementById("match-api");
-
-let matches = data.response;
-
-if(matches.length === 0){
-
-matchBox.innerHTML="❌ Pa gen match jodi a.";
-
+matchBox.innerHTML = "❌ API pa bay repons";
 return;
 
 }
 
-
 matchBox.innerHTML="";
 
-
-matches.slice(0,5).forEach(match=>{
+data.response.slice(0,5).forEach(match=>{
 
 matchBox.innerHTML += `
-
 <div class="match-item">
-
 ⚽ ${match.teams.home.name}
-
 <b> VS </b>
-
 ${match.teams.away.name}
-
 <br>
-
 🕒 ${match.fixture.date}
-
 </div>
-
 <hr>
-
 `;
 
 });
-
 
 }
 
 catch(error){
 
-document.getElementById("match-api").innerHTML=
-"❌ Erè koneksyon API";
+console.log(error);
+
+matchBox.innerHTML="❌ Erè koneksyon API";
 
 }
 
 }
-
 
 montreMatch();
