0ab280b5d7415c2f340d552e1ddaf21a8525b11d
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-22 22:28:02 -0400

    Update api.js

--- api.js
+++ api.js
@@ -32,3 +32,72 @@ console.log("Erè API:", error);
 }
 
   }
+async function montreMatch(){
+
+try{
+
+const response = await fetch(
+API_URL + "/fixtures?date=2026-07-23",
+{
+headers:{
+"x-apisports-key": API_KEY
+}
+}
+);
+
+const data = await response.json();
+
+let matchBox = document.getElementById("match-api");
+
+let matches = data.response;
+
+if(matches.length === 0){
+
+matchBox.innerHTML="❌ Pa gen match jodi a.";
+
+return;
+
+}
+
+
+matchBox.innerHTML="";
+
+
+matches.slice(0,5).forEach(match=>{
+
+matchBox.innerHTML += `
+
+<div class="match-item">
+
+⚽ ${match.teams.home.name}
+
+<b> VS </b>
+
+${match.teams.away.name}
+
+<br>
+
+🕒 ${match.fixture.date}
+
+</div>
+
+<hr>
+
+`;
+
+});
+
+
+}
+
+catch(error){
+
+document.getElementById("match-api").innerHTML=
+"❌ Erè koneksyon API";
+
+}
+
+}
+
+
+montreMatch();
