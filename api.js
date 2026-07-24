bdd1099ee625a4ad1a8300217dc02768b999ce9d
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-23 23:26:10 -0400

    Update api.js

--- api.js
+++ api.js
@@ -110,3 +110,71 @@ console.log(
 
 
 }
+const matchBox = document.getElementById("match-api");
+
+
+const matches = [
+
+{
+home:"⚪ Real Madrid",
+away:"🔵 Barcelona",
+time:"20:00"
+},
+
+{
+home:"🔵 Manchester City",
+away:"🔴 Liverpool",
+time:"18:30"
+},
+
+{
+home:"🔴 PSG",
+away:"⚪ Marseille",
+time:"21:00"
+}
+
+];
+
+
+function loadMatches(){
+
+if(!matchBox){
+return;
+}
+
+
+matchBox.innerHTML="";
+
+
+matches.forEach(match => {
+
+
+matchBox.innerHTML += `
+
+<div class="match-item">
+
+⚽ ${match.home}
+
+<br>
+
+🆚
+
+<br>
+
+${match.away}
+
+<br>
+
+🕒 ${match.time}
+
+</div>
+
+`;
+
+});
+
+
+}
+
+
+loadMatches();
