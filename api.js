bd253f29d1dc2831a347bc978fb8021202fa0069
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-23 23:51:02 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,60 @@
-alert("ANALIZFOOT API OK");
+const matchBox = document.getElementById("match-api");
+
+const matches = [
+{
+home:"⚪ Real Madrid",
+away:"🔵 Barcelona",
+time:"20:00"
+},
+
+{
+home:"🔵 Manchester City",
+away:"🔴 Liverpool",
+time:"18:30"
+},
+
+{
+home:"🔴 PSG",
+away:"⚪ Marseille",
+time:"21:00"
+}
+];
+
+
+function loadMatches(){
+
+if(!matchBox){
+return;
+}
+
+matchBox.innerHTML="";
+
+
+matches.forEach(match => {
+
+matchBox.innerHTML += `
+
+<div class="match-item">
+
+⚽ ${match.home}
+
+<br>
+🆚
+<br>
+
+${match.away}
+
+<br>
+
+🕒 ${match.time}
+
+</div>
+
+`;
+
+});
+
+}
+
+
+loadMatches();
