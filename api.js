7467aa3f4c44f11a84c86c0138bc64d61c179dd3
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-24 00:33:44 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,24 +1,36 @@
 const matchBox = document.getElementById("match-api");
 
+const matchBox = document.getElementById("match-api");
+
+
 const matches = [
+
 {
 home:"⚪ Real Madrid",
 away:"🔵 Barcelona",
-time:"20:00"
+time:"20:00",
+status:"🔴 Live",
+score:"2 - 1"
 },
 
 {
 home:"🔵 Manchester City",
 away:"🔴 Liverpool",
-time:"18:30"
+time:"18:30",
+status:"⏳ Pa kòmanse",
+score:"-"
 },
 
 {
 home:"🔴 PSG",
 away:"⚪ Marseille",
-time:"21:00"
+time:"21:00",
+status:"⏳ Pa kòmanse",
+score:"-"
 }
+
 ];
+
 
 
 function loadMatches(){
@@ -27,26 +39,50 @@ if(!matchBox){
 return;
 }
 
+
 matchBox.innerHTML="";
 
 
-matches.forEach(match => {
+matches.forEach(match=>{
+
 
 matchBox.innerHTML += `
 
 <div class="match-item">
 
-⚽ ${match.home}
 
-<br>
-🆚
-<br>
+<h3>
+${match.status}
+</h3>
 
+
+<p>
+${match.home}
+</p>
+
+
+<strong>
+${match.score}
+</strong>
+
+
+<p>
 ${match.away}
+</p>
 
-<br>
 
+<p>
 🕒 ${match.time}
+</p>
+
+
+
+<div class="button" onclick="location.href='match.html'">
+
+📊 Analize Match
+
+</div>
+
 
 </div>
 
@@ -54,7 +90,9 @@ ${match.away}
 
 });
 
+
 }
+
 
 
 loadMatches();
