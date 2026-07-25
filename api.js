ce168bb25f67dc65062658c5af24658619b6e70a
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-25 00:33:11 -0400

    Update api.js

--- api.js
+++ api.js
@@ -14,7 +14,7 @@ matchBox.innerHTML = "⏳ Ap chèche match yo...";
 try{
 
 const response = await fetch(
-"https://v3.football.api-sports.io/fixtures?next=5",
+"https://v3.football.api-sports.io/fixtures?date=2026-07-25"
 {
 method:"GET",
 headers:{
